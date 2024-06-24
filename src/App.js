6635fdf2a5bc942a208749d5edf14580e36052ea
Vishal Kumar Khatri <kishore@bunity.com>
2024-06-24 17:30:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,124 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import {
+  lowerCasedAlphabets,
+  numbers,
+  symbol,
+  upperCasedAlphabets,
+} from './Data/passChar';
 
 function App() {
+  let [upperCase, setUppercase] = useState(false);
+  let [lowerCase, setLowercase] = useState(false);
+  let [number, setNumber] = useState(false);
+  let [symbols, setSymbols] = useState(false);
+  let [passlength, setPasslength] = useState(10);
+  let [fpass, setFpass] = useState('');
+
+  let createPassword = (e) => {
+    let charSet = '';
+    let finalPass = '';
+    e.preventDefault();
+    if (upperCase || lowerCase || number || symbols) {
+      if (upperCase) charSet += upperCasedAlphabets;
+      if (lowerCase) charSet += lowerCasedAlphabets;
+      if (number) charSet += numbers;
+      if (symbols) charSet += symbol;
+      for (let i = 0; i < passlength; i++) {
+        finalPass += charSet.charAt(Math.floor(Math.random() * charSet.length));
+        setFpass(finalPass);
+      }
+    } else {
+      toast.error('Please select at least one checkbox value...!');
+      // alert('Please select at least one checkbox value...!');
+    }
+  };
+  let copyPass = () => {
+    navigator.clipboard.writeText(fpass);
+    toast.success('Password Copied');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer />
+      <div className="container">
+        <h1 className="heading">Random Password Generator</h1>
+        <form id="passwordForm">
+          <p className="length">Current Length:</p>
+          <div className="flex">
+            <input
+              type="text"
+              className="generated-pass"
+              readOnly
+              value={fpass}
+            />
+            <button
+              type="button"
+              id="copyBtn"
+              className="copyBtn"
+              onClick={copyPass}
+            >
+              Copy Password
+            </button>
+          </div>
+          <div className="flex">
+            {/* <p>10</p> */}
+            {/* <input id="length" type="range" min={10} max={40} /> */}
+            {/* <p>40</p> */}
+            <p>Length</p>
+            <input
+              className=""
+              id="length"
+              type="number"
+              min={10}
+              max={20}
+              value={passlength}
+              onChange={(e) => setPasslength(e.target.value)}
+            />
+          </div>
+          <div className="flex">
+            <label htmlFor="includeUpperCase">Include Uppercase </label>
+            <input
+              id="includeUpperCase"
+              type="checkbox"
+              checked={upperCase}
+              onChange={() => setUppercase(!upperCase)}
+            />
+          </div>
+          <div className="flex">
+            <label htmlFor="includeLowerCase">Include LowerCase </label>
+            <input
+              id="includeLowerCase"
+              type="checkbox"
+              checked={lowerCase}
+              onChange={() => setLowercase(!lowerCase)}
+            />
+          </div>
+          <div className="flex">
+            <label htmlFor="includeNumber">Include Number</label>
+            <input
+              id="includeNumber"
+              type="checkbox"
+              checked={number}
+              onChange={() => setNumber(!number)}
+            />
+          </div>
+          <div className="flex">
+            <label htmlFor="includeSymbols">Include Symbols </label>
+            <input
+              id="includeSymbols"
+              type="checkbox"
+              checked={symbols}
+              onChange={() => setSymbols(!symbols)}
+            />
+          </div>
+          <button type="submit" onClick={createPassword}>
+            Generate Password
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
 
---
+++ src/Data/passChar.jsx
@@ -0,0 +1,4 @@
+export let lowerCasedAlphabets = 'abcdefghijklmnopqrstuvwxyz';
+export let upperCasedAlphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export let numbers = '1234567890';
+export let symbol = '!@#$%^&*()-_+=/?';
